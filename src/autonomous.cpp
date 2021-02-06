f67b5628a165c37c2aa16a57640d5662d714156d
sloth56 <sleuth56@protonmail.com>
2021-02-05 23:28:08 -0500

    auton updates

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -2,20 +2,56 @@
 
 // When set to true driver code stops
 bool isAuton = false;
+bool waitingForBall = false;
+
+void stopForBall(void* param) {
+  while (true) {
+    if (waitingForBall == true && BottomBallDetector.get() < 100) {
+      Intake.moveVelocity(0);
+      waitingForBall = false;
+    }
+    pros::delay(20);
+  }
+}
+
+void waitTillClear() {
+    while (true) {
+    if (TopBallDetector.get() > 100 && BottomBallDetector.get() > 100) {
+      break;
+    }
+    pros::delay(20);
+  }
+}
+
+void moveDistance(okapi::QLength distance, int timeout) {
+     chassis->moveDistanceAsync(distance); // move to the target asynchronously (without waiting)
+     long endTime = pros::millis() + timeout; // determine when to stop if it hasnt settled
+     while(!chassis->isSettled()) { // loop if not settled
+          if(pros::millis() >= endTime) { // if not settled and time has ran out
+              chassis->stop();
+               break; // break the loop and continue with autonomous
+          }
+     }
+}
+
+pros::Task stopForBall_TR(stopForBall, (void*)"PROS", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "My Task");
 
 void blueSmall() {
-  // set the state to zero
-  chassis->setState({0_in, 0_in, 0_deg});
-  Roller.moveVelocity(200); Intake.moveVelocity(500);
-  chassis->driveToPoint({1.5_ft, 0_ft});
-  // pros::delay(200);
-  Roller.moveVelocity(0); Intake.moveVelocity(000); OutTake.moveVelocity(000);
-  chassis->driveToPoint({2.2_ft, -1.7_ft});
-  Roller.moveVelocity(0); Intake.moveVelocity(500); OutTake.moveVelocity(600);
-  pros::delay(500);
-  Roller.moveVelocity(0); Intake.moveVelocity(0); OutTake.moveVelocity(0);
+  // Roller.moveVelocity(200); Intake.moveVelocity(400);
+  // waitingForBall = true;
+
+  // // set the state to zero
+  // chassis->setState({0_in, 0_in, 0_deg});
+  // Roller.moveVelocity(200); Intake.moveVelocity(500);
+  // chassis->driveToPoint({1.5_ft, 0_ft});
+  // // pros::delay(200);
+  // Roller.moveVelocity(0); Intake.moveVelocity(000); OutTake.moveVelocity(000);
+  // chassis->driveToPoint({2.2_ft, -1.7_ft});
+  // Roller.moveVelocity(0); Intake.moveVelocity(500); OutTake.moveVelocity(600);
+  // pros::delay(500);
+  // Roller.moveVelocity(0); Intake.moveVelocity(0); OutTake.moveVelocity(0);
   
-  chassis->driveToPoint({0_ft, 0_ft}, true);
+  // chassis->driveToPoint({0_ft, 0_ft}, true);
   // Roller.moveVelocity(200); Intake.moveVelocity(500);
   // chassis->driveToPoint({3_ft, 4_ft});
   // pros::delay(200);
@@ -27,49 +63,68 @@ void blueSmall() {
 }
 
 void blueSafe() {
-  Roller.moveVelocity(200); Intake.moveVelocity(500); OutTake.moveVelocity(200);
+  Roller.moveVelocity(200); Intake.moveVelocity(600); OutTake.moveVelocity(200);
   while (TopBallDetector.get() > 100 || TopBallDetector.get() == -1) {
     pros::delay(20);
   }
   OutTake.moveVelocity(0);
+  waitingForBall = true;
   chassis->moveDistance(1.7_ft);
-  chassis->turnAngle(137_deg);
-  Roller.moveVelocity(0); Intake.moveVelocity(000); OutTake.moveVelocity(000);
-  chassis->moveDistance(2.1_ft);
-  Roller.moveVelocity(0); Intake.moveVelocity(500); OutTake.moveVelocity(600);
-  pros::delay(500);
+  Roller.moveVelocity(0); OutTake.moveVelocity(0);  
+  chassis->turnAngle(132_deg);
+  moveDistance(2.3_ft, 2300);
+  Intake.moveVelocity(600); OutTake.moveVelocity(600);
+  pros::delay(600);
   Roller.moveVelocity(0); Intake.moveVelocity(0); OutTake.moveVelocity(0);
 
   chassis->moveDistance(-1.2_ft);
-  chassis->turnAngle(-135_deg);
-  Roller.moveVelocity(200); Intake.moveVelocity(300); OutTake.moveVelocity(400);
+  chassis->turnAngle(-129_deg);
+  Roller.moveVelocity(200); Intake.moveVelocity(600); OutTake.moveVelocity(200);
   while (TopBallDetector.get() > 100 || TopBallDetector.get() == -1) {
     pros::delay(20);
   }
   OutTake.moveVelocity(0);
-  chassis->moveDistance(4_ft);
-  while (BottomBallDetector.get() > 100 || BottomBallDetector.get() == -1) {
-    pros::delay(20);
-  }
-  Roller.moveVelocity(0); Intake.moveVelocity(0); OutTake.moveVelocity(0);
-  chassis->turnAngle(95_deg);
-  chassis->moveDistance(0.6_ft);
-  Roller.moveVelocity(0); Intake.moveVelocity(500); OutTake.moveVelocity(500);
-  pros::delay(1400);
-  Roller.moveVelocity(0); Intake.moveVelocity(0); OutTake.moveVelocity(0);
+  chassis->moveDistance(4.2_ft);
+  pros::delay(550);
+  Roller.moveVelocity(0); OutTake.moveVelocity(0); Intake.moveVelocity(0);
+  chassis->turnAngle(93_deg);
+  Roller.moveVelocity(0);
+  moveDistance(1_ft, 2000);
+  Roller.moveVelocity(200); Intake.moveVelocity(500); OutTake.moveVelocity(500);
+  waitTillClear();
+  Roller.moveVelocity(0);
+  pros::delay(2000);
+  Roller.moveVelocity(-200); Intake.moveVelocity(0); OutTake.moveVelocity(0);
   
   chassis->moveDistance(-1.8_ft);
-  chassis->turnAngle(-99_deg);
-  Roller.moveVelocity(200); Intake.moveVelocity(400); OutTake.moveVelocity(300);
+  Roller.moveVelocity(0);
+  chassis->turnAngle(-88_deg);
+  Roller.moveVelocity(200); Intake.moveVelocity(300); OutTake.moveVelocity(200);
   chassis->moveDistance(4_ft);
-  while (TopBallDetector.get() > 100 || TopBallDetector.get() == -1) {
+  int timeout = pros::millis() + 10000;
+  while (TopBallDetector.get() > 100 && timeout > pros::millis()) {
     pros::delay(20);
   }
   Roller.moveVelocity(0); Intake.moveVelocity(0); OutTake.moveVelocity(0);
-  chassis->turnAngle(68_deg);
-  chassis->moveDistance(2.1_ft);
+  chassis->turnAngle(64_deg);
+  moveDistance(2.5_ft, 3000);
   Roller.moveVelocity(0); Intake.moveVelocity(500); OutTake.moveVelocity(500);
-  pros::delay(600);
+  while (TopBallDetector.get() > 100 || BottomBallDetector.get() > 100) {
+    pros::delay(20);
+  }
+  pros::delay(1000);
+  chassis->moveDistance(-2_ft);
+  Roller.moveVelocity(0); Intake.moveVelocity(0); OutTake.moveVelocity(0);
+
+  chassis->turnAngle(-157_deg);
+  Roller.moveVelocity(200); Intake.moveVelocity(600);
+  waitingForBall = true;
+  chassis->moveDistance(6_ft);
+  chassis->turnAngle(44_deg);
+  Roller.moveVelocity(0); Intake.moveVelocity(0); OutTake.moveVelocity(0);
+  moveDistance(2_ft, 2000);
+  Roller.moveVelocity(200); Intake.moveVelocity(600); OutTake.moveVelocity(600);
+  pros::delay(2000);
   chassis->moveDistance(-2_ft);
   Roller.moveVelocity(0); Intake.moveVelocity(0); OutTake.moveVelocity(0);
 }
